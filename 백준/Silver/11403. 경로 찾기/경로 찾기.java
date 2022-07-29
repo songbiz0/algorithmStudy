ad214f93c2b6ea00d7f7aee04a47b1df7d802532
Minjae Kim <songbiz00@gmail.com>
2022-07-29 22:52:15 +0900

    [Silver I] Title: 경로 찾기, Time: 244 ms, Memory: 24452 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] nodes;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        nodes = new int[N][N];
+        for(int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++) {
+                nodes[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i=0; i<N; i++) {
+            search(i);
+        }
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                sb.append(nodes[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString().trim());
+    }
+
+    public static void search(int a) {
+        boolean[] visited = new boolean[N];
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0; i<N; i++) {
+            if(nodes[a][i] == 1) {
+                q.offer(i);
+            }
+        }
+        while(!q.isEmpty()) {
+            int next = q.poll();
+            if(visited[next]) {
+                continue;
+            }
+            visited[next] = true;
+            for(int i=0; i<N; i++) {
+                if(nodes[next][i] == 1 && !visited[i]) {
+                    q.offer(i);
+                    nodes[a][i] = 1;
+                }
+            }
+        }
+    }
+}
